5b6e31ca1542779f3029ab5126912248182a8ab9
caste <caste2593@gmail.com>
2020-02-08 13:04:56 +0100

    segundo

---
+++ src/main/java/net/carlosPracticas/app/controller/noticiasController.java
@@ -0,0 +1,25 @@
+package net.carlosPracticas.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/noticias")
+public class noticiasController {
+
+    @GetMapping(value="/create")
+    public String crear(){
+        return "noticias/formNoticia";
+
+    }
+@PostMapping(value="/save")
+public String guardar(@RequestParam("titulo") String titulo){
+        return "noticias/formNoticia";
+
+
+}
+
+}
